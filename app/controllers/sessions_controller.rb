05a5f5fd0123ec7a890fb4c8fd5ed198ca72dfb4
Jon Greenspan <jgreenspan10@gmail.com>
2015-11-19 15:14:32 -0500

    rerouted sign in to user#show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	    user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to restaurants_url, :notice => "Logged in!"
+      redirect_to user_path(user)
     else
     	flash.now[:alert] = "Invalid email or password"
       render "new"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	def create
   	@user = User.new(user_params)
   	  if @user.save
-  		redirect_to restaurants_url, notice: "Hello, #{@user.name}!"
+  		redirect_to user_path(@user), notice: "Hello, #{@user.name}!"
   	  else 
   		render "new"
   	  end
