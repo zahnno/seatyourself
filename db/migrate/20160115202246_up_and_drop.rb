7d717c1cf3b247310de98aaa7243c4e5dcf3cd1b
PatrickZanowski <zanowski26@hotmail.com>
2016-01-15 15:24:55 -0500

    Changed restime column

---
+++ db/migrate/20160115202246_up_and_drop.rb
@@ -0,0 +1,14 @@
+class UpAndDrop < ActiveRecord::Migration
+  def change
+  end
+
+  def up
+    remove_column :reservations, :restime
+    add_column :reservations, :restime, :datetime
+  end
+
+  def down
+    remove_column :reservations, :restime
+    add_column :reservations, :restime, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115201237) do
+ActiveRecord::Schema.define(version: 20160115202246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
